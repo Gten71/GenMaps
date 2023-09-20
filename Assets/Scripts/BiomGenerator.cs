22531d34c0571a47291c82fee923fccf86c07e84
Maxim <mresnyansky@gmail.com>
2023-09-20 11:25:00 +0200

    first up date

--- Assets/Scripts/BiomGenerator.cs
+++ Assets/Scripts/BiomGenerator.cs
@@ -1,4 +1,6 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class BiomGenerator : MonoBehaviour
 {
@@ -6,26 +8,39 @@ public class BiomGenerator : MonoBehaviour
     public int mapSizeX;
     public int mapSizeY;
     public float biomSize; // Размер каждого квадрата биома в метрах
+    private BiomInfo[,] biomInfoMap;
+
 
     private GameObject[,] biomMap;
     
 
-    private void GenerateBiomMap()
+
+    public void GenerateBiomMap()
     {
-        biomMap = new GameObject[mapSizeX, mapSizeY];
+        biomInfoMap = new BiomInfo[mapSizeX, mapSizeY];
 
         for (int x = 0; x < mapSizeX; x++)
         {
             for (int y = 0; y < mapSizeY; y++)
             {
                 GameObject biomPrefab = biomPrefabs[Random.Range(0, biomPrefabs.Length)];
-                
+
                 Vector3 position = new Vector3(x * biomSize, y * biomSize, 0f);
+
+                // Создаем объект BiomInfo и сохраняем его в массиве biomInfoMap
+                BiomInfo biomInfo = new BiomInfo
+                {
+                    biomPrefab = biomPrefab,
+                    position = position
+                };
+
+                biomInfoMap[x, y] = biomInfo;
                 
                 GameObject biomInstance = Instantiate(biomPrefab, position, Quaternion.identity);
-                
-                biomMap[x, y] = biomInstance;
+                Debug.Log($"Сохранено: Биом {biomPrefab.name} на позиции ({x}, {y})");
+
             }
         }
     }
+
 }
---
+++ Assets/Scripts/BiomInfo.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BiomInfo
+{
+    public GameObject biomPrefab;
+    public Vector3 position;
+}
